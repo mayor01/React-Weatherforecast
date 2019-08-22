602d6323b96d85a6121e44721de3cfff702e7cb9
mayor01 <mayor01@users.noreply.github.com>
2019-08-22 22:32:11 +0100

    second commit

--- src/components/chart.js
+++ src/components/chart.js
@@ -9,7 +9,7 @@ function average(data) {
 export default (props) => {
     return (
       <div>
-        <Sparklines height={120} width={180} data={props.data}>
+        <Sparklines data={props.data}>
           <SparklinesLine color={props.color} />
           <SparklinesReferenceLine type="avg" />
         </Sparklines>
@@ -17,3 +17,4 @@ export default (props) => {
       </div>
     );
 }
+//height={120} width={180} 
